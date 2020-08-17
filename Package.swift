4f6193e7c202c10a1e96a7a1be978216432ee7f0
David Martin Saiz <davms81@gmail.com>
2020-08-17 18:07:21 +0200

    Set iOS version to 11 instead 9

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SimplyLogger",
     platforms: [
           // Add support for all platforms starting from a specific version.
-          .iOS(.v9),
+          .iOS(.v11),
       ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
