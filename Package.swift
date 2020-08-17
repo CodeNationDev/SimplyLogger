157f86d9a8267fd41ee242543c488c27332882cd
David Martin Saiz <davms81@gmail.com>
2020-08-17 18:02:13 +0200

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SimplyLogger",
+    platforms: [
+          // Add support for all platforms starting from a specific version.
+          .iOS(.v9),
+      ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SimplyLogger",
+            targets: ["SimplyLogger"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "SimplyLogger",
+            dependencies: [])
+    ]
+)
