40308e564afa863edc20863bae31ab8aee82709f
David Martin Saiz <davms81@gmail.com>
2020-08-22 16:45:03 +0200

    Add compatibility with macOS, watchOS and tvOS

--- Package.swift
+++ Package.swift
@@ -8,6 +8,9 @@ let package = Package(
     platforms: [
           // Add support for all platforms starting from a specific version.
           .iOS(.v11),
+          .tvOS(.v11),
+          .watchOS(.v4),
+          .macOS(.v10_12),
       ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
